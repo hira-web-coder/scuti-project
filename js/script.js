3c263345c97ad7cdfa9124b2a5178c4846098262
hira-web-coder <webcoderhira@gmail.com>
2025-05-15 00:39:07 +0600

    slider and testimonial

--- js/script.js
+++ js/script.js
@@ -2,5 +2,30 @@ $('.counter').counterUp({
 delay:10,
 time:500,
 })
+// ---silk slider-----------
 
-  AOS.init();
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true
+});
+// ----testimonials----
+$('.my_slider_2').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<i class="fa-solid arrow1 fa-arrow-left"></i>',
+  nextArrow: "<i class='fa-solid arrow2 fa-arrow-right'></i>"
+});
+	
+	
