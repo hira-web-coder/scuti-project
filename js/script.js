f1b8d1326df3e578bb67356772d89d57d1fb00af
hira-web-coder <webcoderhira@gmail.com>
2025-05-18 21:47:24 +0600

    responsive for Extra small and small

--- js/script.js
+++ js/script.js
@@ -23,7 +23,37 @@ $('.my_slider_2').slick({
   slidesToShow: 3,
   slidesToScroll: 3,
   prevArrow: '<i class="fa-solid arrow1 fa-arrow-left"></i>',
-  nextArrow: "<i class='fa-solid arrow2 fa-arrow-right'></i>"
+  nextArrow: "<i class='fa-solid arrow2 fa-arrow-right'></i>",
+ responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
+// ---aos animation---
+ AOS.init();
 	
 	
